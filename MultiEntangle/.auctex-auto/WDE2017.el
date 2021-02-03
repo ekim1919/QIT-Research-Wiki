cc67189cfabeffa2f86918b980ad841718a468bd
Edward Kim <edbeta12@gmail.com>
2021-02-03 04:04:12 -0500

    References from Walter et al. survey

---
+++ MultiEntangle/.auctex-auto/WDE2017.el
@@ -0,0 +1,50 @@
+(TeX-add-style-hook
+ "WDE2017"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org289b808"
+    "sec:org6ca10d4"
+    "sec:org7e31bcb"
+    "sec:org0d4c8a7"
+    "sec:org6ff7435"
+    "sec:org7757441"
+    "sec:org08b2af6"
+    "sec:orgc07a1c9"
+    "sec:org8a5d312"
+    "sec:org467692a"
+    "sec:org654c442"
+    "sec:org4039b25"
+    "sec:org739ffae"
+    "sec:org05c3381"
+    "sec:org2c1d95b"))
+ :latex)
+
